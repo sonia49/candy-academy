57e025dd57a662ce7b6e1fd176bfc988981d02ae
sonia49 <soniamezdari@gmail.com>
2026-01-30 14:46:42 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/candy-academy/', // <--- VERIFIE BIEN CETTE LIGNE
+  base: '/candy-academy/', 
 })
